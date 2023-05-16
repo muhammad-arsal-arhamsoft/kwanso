e0e06d286145bb885882e00f974f6be82e991d83
Arsal <arsal3414@gmail.com>
2023-05-17 00:11:24 +0500

    arsal

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(){
+        return "register";
+    }
+
+
+
+    
+    public function listTasks(){
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/list-tasks');
+
+Route::get('/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
+
+Route::get('/login', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'register']);
+
+Route::get('/user', [UserController::class, 'register']);
+
+Route::get('/list-tasks', [UserController::class, 'listTasks']);
